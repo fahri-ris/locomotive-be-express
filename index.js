5819d49767a500fe6d5f6250895b92010d6ad634
Fahri Ris <fahri.ris15@gmail.com>
2024-09-17 22:08:01 +0700

    [DB] mongodb connection

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// mongodb connection
+mongoose.connect('mongodb://localhost:27017/locomotive');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port: ${PORT}`);
+})
